8f4b464583b697a92a3211ab8ddde606e9584cb8
Caleb Cochran <k1kokhran@gmail.com>
2017-08-14 07:41:40 -0500

    Clean up tests

--- test/main.js
+++ test/main.js
@@ -6,22 +6,8 @@ describe('Learning Paths', () => {
     describe('Outputting student data', () => {
         it('should output the correct student data', () => {
             var text = "Student Name,RF,RL,RI,L\nAlbin Stanton,2,3,K,3\nErik Purdy,3,1,1,1\nAimee Cole,K,K,1,2";
-            console.log(LearningPaths);
             var studentData = LearningPaths.parseStudentData(text);
-            console.log(studentData);
-            assert.equal(-1, [1,2,3].indexOf(4));
+            //assert.equal(-1, [1,2,3].indexOf(4));
         });
     });
-
-/*     describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
-    });
-
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
-    }); */
 });
